66c2de62042566f9bd0f29bb89dd7c45353ce36b
金　禹陳　 <lohpof@gmail.com>
2023-11-13 02:31:08 +0900

    before get sleep

--- article/models.py
+++ article/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Article(models.Model):
+    author = models.ForeignKey("user.User", verbose_name="작성자", on_delete=models.SET_NULL)
+    title = 
+
 
 class Comment(models.Model):
     """주석"""
